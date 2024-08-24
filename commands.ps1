fd9517684a3f8397ff2d1853574b025cfd69dcfb
KinggMustafa <127482601+KinggMustafa@users.noreply.github.com>
2024-08-24 16:26:42 -0400

    Update commands.ps1

--- commands.ps1
+++ commands.ps1
@@ -1,7 +1,7 @@
-# URL for generating stories from your FastAPI application
+
 $url = "http://localhost:8001/generate_stories/"
 
-# Headers for the POST request (adjust if you have different headers)
+
 $headers = @{ "Content-Type" = "application/json" }
 
 # Execute the POST request to generate stories
